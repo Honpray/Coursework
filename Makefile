cc2dffb7ebe49189bdd4b378aed9a9be14b6e83e
Honpray <hanbing.leng@sjsu.edu>
2018-04-16 18:44:12 -0700

    finish listing task kernel module

--- Makefile
+++ Makefile
@@ -1,4 +1,6 @@
-obj-m += birthday.o
+#obj-m += birthday_mod.o
+#obj-m += listing_task_mod.o
+obj-m += listing_task_dfs_mod.o
 all:
 	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules
 clean:
--- listing_task_dfs_mod.c
+++ listing_task_dfs_mod.c
@@ -3,24 +3,30 @@
 #include <linux/init.h>		/* Needed for the macros */
 #include <linux/sched.h>	/* Nedded for the for_each_process() macro*/
 
-/*
+/* defined in struct task_struct
 char comm[TASK_COMM_LEN];
 pid_t pid;
 volatile long state;
-
 struct list_head children;
 struct list_head sibling;
 */
 
+void helper(struct task_struct *task){
+	struct task_struct *next_task;
+	struct list_head *list; 
+	printk(KERN_INFO "%-15s%6d%6ld", task->comm, task->pid, task->state);
+	list_for_each(list, &task->children) {
+		next_task = list_entry(list, struct task_struct, sibling);
+		/* next_task points to the next child in the list */
+		helper(next_task);	
+	}	
+}
+
 static int __init listing_task_init(void)
 {
 	printk(KERN_INFO "Hello world\n");
-	struct task_struct *task;
 	printk(KERN_INFO "Process Name      PID State");
-	for_each_process(task) {
-		/* on each iteration task points to the next task */
-		printk(KERN_INFO "%-15s%6d%6ld", task->comm, task->pid, task->state); 		
-	}
+	helper(&init_task);
 	return 0;
 }
 
