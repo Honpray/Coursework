01e683e81d5f3d8fd9c6bcdd2193ea9f558433d8
Honpray <hanbing.leng@sjsu.edu>
2018-10-30 16:49:21 -0700

    add VulkanTesting

---
+++ VulkanTesting/shaders/compile.sh
@@ -0,0 +1,2 @@
+/Users/hanbingleng/Documents/vulkansdk-macos-1.1.77.0/macOS/bin/glslangValidator -V shader.vert
+/Users/hanbingleng/Documents/vulkansdk-macos-1.1.77.0/macOS/bin/glslangValidator -V shader.frag
